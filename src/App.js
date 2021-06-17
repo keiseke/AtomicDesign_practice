01c87f319696e4c3e7954441965b6f98b375223f
keiseke <81750996+keiseke@users.noreply.github.com>
2021-06-17 22:28:00 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import { PrimaryButton } from "./components/atom/button/PrimaryButton";
-import { SecondaryButton } from "./components/atom/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "みや",
-  image: "https://source.unsplash.com/photos/r-xKieMqL34",
-  email: "12345@gmail.com",
-  phone: "080-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https//google.com"
-};
-
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
